f0260a161dc896e6873cc18c787e66f1a163bb18
ahmed <ABSSANE.AHMED@ofppt-edu.ma>
2022-08-27 13:59:46 +0100

    update

--- js/checkout.js
+++ js/checkout.js
@@ -5,10 +5,7 @@ window.onload = function () {
 
   document.getElementById("offre").innerHTML = offre;
   document.getElementById("price").innerHTML = price;
-<<<<<<< HEAD
 
-=======
   document.getElementById("offree").value = offre;
   document.getElementById("pricee").value = price;
->>>>>>> d054e9f37dcdd9a2b313211a53195e4137aa68e0
 };
